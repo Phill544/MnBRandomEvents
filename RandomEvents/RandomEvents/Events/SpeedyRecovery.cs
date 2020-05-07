b88dd01afe1eb349a01ef09dfb25b0fac8422294
Phill Moon <philljm544@gmail.com>
2020-05-07 21:09:05 +1000

    Fix incorrect exception text

--- RandomEvents/RandomEvents/Events/SpeedyRecovery.cs
+++ RandomEvents/RandomEvents/Events/SpeedyRecovery.cs
@@ -72,7 +72,7 @@ namespace CryingBuffalo.RandomEvents.Events
 			}
 			catch (Exception ex)
 			{
-				MessageBox.Show($"Error while stopping \"{this.RandomEventData.EventType}\" event :\n\n {ex.Message} \n\n { ex.StackTrace}");
+				MessageBox.Show($"Error while playing \"{this.RandomEventData.EventType}\" event :\n\n {ex.Message} \n\n { ex.StackTrace}");
 			}
 		}
 
