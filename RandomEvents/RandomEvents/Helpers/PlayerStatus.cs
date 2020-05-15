a04672ace1d19f74ebf0e69ab26543b5f9a5baef
Phill Moon <philljm544@gmail.com>
2020-05-15 17:40:56 +1000

    Add new helper classes

---
+++ RandomEvents/RandomEvents/Helpers/PlayerStatus.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.CampaignSystem;
+
+namespace CryingBuffalo.RandomEvents.Helpers
+{
+	public static class PlayerStatus
+	{
+		/// <summary>
+		/// Checks whether the player currently owns any settlements.
+		/// </summary>
+		public static bool HasSettlement()
+		{
+			if (Hero.MainHero.Clan.Settlements.Count() > 0)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+}
---
+++ RandomEvents/RandomEvents/Helpers/RandomSelection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.Core;
+
+namespace CryingBuffalo.RandomEvents.Helpers
+{
+	public static class RandomSelection<T>
+	{
+		public static T GetRandomElement(IEnumerable<T> enumerable)
+		{
+			return enumerable.ElementAt(MBRandom.RandomInt(0, enumerable.Count()));
+		}
+	}
+}
