f9791dc4c94ecf4e3484c82073bde7cb9e8c5c37
Phill Moon <philljm544@gmail.com>
2022-11-01 02:40:45 +1100

    Temporarily remove the local settings files to aid with Steam workshop support (as it doesn't let us write to the same path as Nexus mods did)

--- RandomEvents/RandomEvents/Settings/Settings.cs
+++ RandomEvents/RandomEvents/Settings/Settings.cs
@@ -12,34 +12,12 @@ namespace CryingBuffalo.RandomEvents.Settings
 
 		public static void LoadRandomEventSettings(bool updateJsonFile = true)
 		{
-			string path = BasePath.Name + "Modules/RandomEvents/ModuleData/RandomEventSettings.json";
-			if (!File.Exists(path))
-			{
-				string defaultSettingsText = JsonConvert.SerializeObject(new RandomEventSettings(), Formatting.Indented);
-				File.WriteAllText(path, defaultSettingsText);
-			}
-			RandomEvents = JsonConvert.DeserializeObject<RandomEventSettings>(File.ReadAllText(path));
-
-			if (!updateJsonFile) return;
-			string updatedSettingsText = JsonConvert.SerializeObject(RandomEvents, Formatting.Indented);
-			File.WriteAllText(path, updatedSettingsText);
+			RandomEvents = new RandomEventSettings();
 		}
 
 		public static void LoadGeneralSettings(bool updateJsonFile = true)
 		{
-			string path = BasePath.Name + "Modules/RandomEvents/ModuleData/GeneralSettings.json";
-			if (!File.Exists(path))
-			{
-				string defaultSettingsText = JsonConvert.SerializeObject(new GeneralSettings(), Formatting.Indented);
-				File.WriteAllText(path, defaultSettingsText);
-			}
-			GeneralSettings = JsonConvert.DeserializeObject<GeneralSettings>(File.ReadAllText(path));
-
-			if (updateJsonFile)
-			{
-				string updatedSettingsText = JsonConvert.SerializeObject(GeneralSettings, Formatting.Indented);
-				File.WriteAllText(path, updatedSettingsText);
-			}
+			GeneralSettings = new GeneralSettings();
 		}
 	}
 }
