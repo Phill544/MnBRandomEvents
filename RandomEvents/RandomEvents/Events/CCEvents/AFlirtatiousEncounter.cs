1706663c15d8a0f6f7859973fc372ee78b53018c
ClearanceClarence <adrian.thomassen92@gmail.com>
2022-11-12 22:22:55 +0100

    Small update to AFlirtatiousEncounter.cs Updated MCM to 5.2.0

--- RandomEvents/RandomEvents/Events/CCEvents/AFlirtatiousEncounter.cs
+++ RandomEvents/RandomEvents/Events/CCEvents/AFlirtatiousEncounter.cs
@@ -9,6 +9,8 @@ using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.Party;
 using TaleWorlds.CampaignSystem.Settlements;
 using TaleWorlds.CampaignSystem.Settlements.Locations;
+using TaleWorlds.CampaignSystem.TournamentGames;
+using TaleWorlds.Core;
 using TaleWorlds.Library;
 
 //New type of event I will attempt to make once the MCM is in place.
@@ -41,14 +43,37 @@ namespace CryingBuffalo.RandomEvents.Events.CCEvents
             
             //PHILL
             //What is needed her is the following
-            //Get if the main character is male or female.
             //Get all heroes and notables in the settlement.
             //Make a new list with characters that are the opposite gender.
             //Then pick a random character that fits.
             //Random variable for pregnancy chance to occur.
             //Event ends with either a NPC gets pregnant or not.
+
+            var notables = Settlement.CurrentSettlement.Notables;
+            var heroes = Settlement.CurrentSettlement.HeroesWithoutParty;
             
-                
+            var characters = notables.Concat(heroes).Distinct().ToList();
+
+            List<Hero> femaleList = new List<Hero>();
+
+            foreach (var character in characters)
+            {
+                if (character.IsFemale)
+                {
+                    femaleList.Add(character);
+                }
+            }
+
+            var random = new Random();
+            int index = random.Next(femaleList.Count);
+
+            var target = femaleList[index];
+
+            var text = $"Your woman is {target.Name}. Enjoy!";
+            
+            InformationManager.DisplayMessage(new InformationMessage(text, RandomEventsSubmodule.Msg_Color));
+            
+
             StopEvent();
         }
 
