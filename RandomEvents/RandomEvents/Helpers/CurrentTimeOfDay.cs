37f7f0c8a034992bae8b2b6f865cbe2037729d2c
ClearanceClarence <29264084+ClearanceClarence@users.noreply.github.com>
2023-12-20 19:33:24 +0100

    Refactored CurrentTimeOfDay.cs to be more logical

--- RandomEvents/RandomEvents/Helpers/CurrentTimeOfDay.cs
+++ RandomEvents/RandomEvents/Helpers/CurrentTimeOfDay.cs
@@ -4,41 +4,14 @@ namespace Bannerlord.RandomEvents.Helpers
 {
     public abstract class CurrentTimeOfDay
     {
-        public static bool IsNight
-        {
-            get
-            {
-                var num = CampaignTime.Now.GetHourOfDay;
-                return num < 6;
-            }
-        }
+        private static int CurrentHour => CampaignTime.Now.GetHourOfDay;
 
+        public static bool IsNight => CurrentHour < 6;
 
-        public static bool IsMorning
-        {
-            get
-            {
-                var num = CampaignTime.Now.GetHourOfDay;
-                return num >= 6 && num < 12;
-            }
-        }
-        
-        public static bool IsMidday
-        {
-            get
-            {
-                var num = CampaignTime.Now.GetHourOfDay;
-                return num >= 12 && num < 18;
-            }
-        }
-        
-        public static bool IsEvening
-        {
-            get
-            {
-                var num = CampaignTime.Now.GetHourOfDay;
-                return num >= 18;
-            }
-        }
+        public static bool IsMorning => CurrentHour >= 6 && CurrentHour < 12;
+
+        public static bool IsMidday => CurrentHour >= 12 && CurrentHour < 18;
+
+        public static bool IsEvening => CurrentHour >= 18;
     }
 }
