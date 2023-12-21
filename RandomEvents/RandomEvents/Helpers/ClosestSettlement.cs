1c06f7557602617c210e0fe6c4639896a29d5092
ClearanceClarence <29264084+ClearanceClarence@users.noreply.github.com>
2023-12-21 13:47:46 +0100

    Small tweak and enhancments - AFlirtatiousEncounter.cs has been restructured with many more variations as a test - Refactored ClosestSettlement.cs - Added new strings in std_module_strings_xml.xml

--- RandomEvents/RandomEvents/Helpers/ClosestSettlement.cs
+++ RandomEvents/RandomEvents/Helpers/ClosestSettlement.cs
@@ -9,95 +9,43 @@ namespace Bannerlord.RandomEvents.Helpers
 {
     public static class ClosestSettlements
     {
-        
-        public static Settlement GetClosestAny(MobileParty heroParty)
+        private static Settlement GetClosestSettlement(MobileParty heroParty, Func<Settlement, bool> condition)
         {
-            Settlement closestSettlement = null;
-            
             try
             {
-                var settlements = Settlement.FindAll(settlement => settlement.IsTown || settlement.IsCastle || settlement.IsVillage).ToList();
-                
-                closestSettlement = settlements.MinBy(settlement => heroParty.GetPosition().DistanceSquared(settlement.GetPosition()));
+                var settlements = Settlement.FindAll(condition).ToList();
+                return settlements.MinBy(settlement => heroParty.GetPosition().DistanceSquared(settlement.GetPosition()));
             }
             catch (Exception ex)
             {
-                MessageBox.Show($"Error when trying to find the closest settlement :\n\n {ex.Message} \n\n { ex.StackTrace}");
+                MessageBox.Show($"Error when trying to find the closest settlement :\n\n {ex.Message} \n\n {ex.StackTrace}");
+                return null;
             }
-            return closestSettlement;
-        }
-        
-        
-        public static Settlement GetClosestTown(MobileParty heroParty)
-        {
-            Settlement closestSettlement = null;
-            
-            try
-            {
-                var settlements = Settlement.FindAll(settlement => settlement.IsTown).ToList();
-                
-                closestSettlement = settlements.MinBy(settlement => heroParty.GetPosition().DistanceSquared(settlement.GetPosition()));
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Error when trying to find the closest town :\n\n {ex.Message} \n\n { ex.StackTrace}");
-            }
-            return closestSettlement;
-        }
-        
-        
-        public static Settlement GetClosestCastle(MobileParty heroParty)
-        {
-            Settlement closestSettlement = null;
-            
-            try
-            {
-                var settlements = Settlement.FindAll(settlement => settlement.IsCastle).ToList();
-                
-                closestSettlement = settlements.MinBy(settlement => heroParty.GetPosition().DistanceSquared(settlement.GetPosition()));
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Error when trying to find the closest castle :\n\n {ex.Message} \n\n { ex.StackTrace}");
-            }
-            return closestSettlement;
-        }
-        
-        
-        public static Settlement GetClosestVillage(MobileParty heroParty)
-        {
-            Settlement closestSettlement = null;
-            
-            try
-            {
-                var settlements = Settlement.FindAll(settlement => settlement.IsVillage).ToList();
-                
-                closestSettlement = settlements.MinBy(settlement => heroParty.GetPosition().DistanceSquared(settlement.GetPosition()));
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Error when trying to find the closest village :\n\n {ex.Message} \n\n { ex.StackTrace}");
-            }
-            return closestSettlement;
-        }
-        
-        
-        public static Settlement GetClosestTownOrVillage(MobileParty heroParty)
-        {
-            Settlement closestSettlement = null;
-            
-            try
-            {
-                var settlements = Settlement.FindAll(settlement => settlement.IsTown || settlement.IsVillage).ToList();
-                
-                closestSettlement = settlements.MinBy(settlement => heroParty.GetPosition().DistanceSquared(settlement.GetPosition()));
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Error when trying to find the closest town or village :\n\n {ex.Message} \n\n { ex.StackTrace}");
-            }
-            return closestSettlement;
         }
 
+        public static Settlement GetClosestAny(MobileParty heroParty)
+        {
+            return GetClosestSettlement(heroParty, settlement => settlement.IsTown || settlement.IsCastle || settlement.IsVillage);
+        }
+
+        public static Settlement GetClosestTown(MobileParty heroParty)
+        {
+            return GetClosestSettlement(heroParty, settlement => settlement.IsTown);
+        }
+
+        public static Settlement GetClosestCastle(MobileParty heroParty)
+        {
+            return GetClosestSettlement(heroParty, settlement => settlement.IsCastle);
+        }
+
+        public static Settlement GetClosestVillage(MobileParty heroParty)
+        {
+            return GetClosestSettlement(heroParty, settlement => settlement.IsVillage);
+        }
+
+        public static Settlement GetClosestTownOrVillage(MobileParty heroParty)
+        {
+            return GetClosestSettlement(heroParty, settlement => settlement.IsTown || settlement.IsVillage);
+        }
     }
 }
